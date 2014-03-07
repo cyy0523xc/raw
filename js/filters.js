1bcf238d0f1bf6862c07e6df530fc9ca5f07b967
Alex Cai <cyy0523xc@gmail.com>
2014-03-07 11:57:53 +0800

    chinese

--- js/filters.js
+++ js/filters.js
@@ -9,8 +9,16 @@ angular.module('raw.filters', [])
     }
 });
 
-angular.module('raw.filters', [])
-.filter('field_type_chn', function(type_en) {
-    var ft_chn = {'NUMBER':'数字', 'STRING':'字符串'};
-    return ft_chn[type_en];
-});
+//angular.module('raw.filters', [])
+//.filter('field_type_chn', function(type_en) {
+//    var ft_chn = {'NUMBER':'数字', 'STRING':'字符串'};
+//    return ft_chn[type_en];
+//});
+
+
+raw.filter('field_type_chn', function() {
+    return function(type_en) {
+        var ft_chn = {'NUMBER':'数字', 'STRING':'字符串'};
+        return ft_chn[type_en];
+    }
+})
