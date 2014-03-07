5b6ac9b8690a05af39fdd848ad46948d5e3d204a
Alex Cai <cyy0523xc@gmail.com>
2014-03-07 11:58:45 +0800

    filter

--- js/filters.js
+++ js/filters.js
@@ -16,7 +16,7 @@ angular.module('raw.filters', [])
 //});
 
 
-raw.filter('field_type_chn', function() {
+app.filter('field_type_chn', function() {
     return function(type_en) {
         var ft_chn = {'NUMBER':'数字', 'STRING':'字符串'};
         return ft_chn[type_en];
